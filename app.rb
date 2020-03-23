d3e95831f180a957c9a9dbf5e347f1469f729803
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-23 19:39:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,4 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+  
+  get "/multiply/:num1/:num2" do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    @product = @num1 * @num2
+    "#{@product}.to_s"
+  end
 end
