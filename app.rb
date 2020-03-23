99c844fed923f72c4a2410587923e72957c30d3a
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-23 19:54:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ get "/reversename/:name" do
    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
   
-  get "/operation/:number1/:number2" do
+  get "/:operation/:number1/:number2" do
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
     @operation = @number1 + @number2
